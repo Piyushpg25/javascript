d3f973f54ce5f18ab39256a12015e88a7f3800a8
Piyush  Gajghate <131654427+Piyushpg25@users.noreply.github.com>
2024-11-04 13:49:16 +0000

    var.js

---
+++ datatypes.js
@@ -0,0 +1,10 @@
+let name="piyush"
+let age= 19
+const idno = 234561
+let isLoggedIn = true/false
+
+console.log(isLoggedIn);
+
+console.log([name,age,idno,isLoggedIn]);
+
+console.log(typeof null);
---
+++ var.js
@@ -0,0 +1,14 @@
+const accountId = 15578
+let account_email = "piyus3@gmail.com"
+var accountPassword ="pg4563737"
+accountcity = "nagpur"
+
+// accountId=55 
+account_email="hgdg@hc"
+accountPassword="dljkndshhl"
+accountcity="mumbai"
+let accountstate;
+
+console.log(accountId);
+
+console.table([accountId,account_email,accountPassword,accountcity,accountstate])
