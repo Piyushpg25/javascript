c41fa8b849efd7c4838d1fc48c5a2b8f8f45d63c
Piyush  Gajghate <131654427+Piyushpg25@users.noreply.github.com>
2024-11-04 14:12:42 +0000

    typescon.js

---
+++ typescon.js
@@ -0,0 +1,19 @@
+let age= true/false
+
+console.log(typeof age);
+
+let ValueInNumber= Number(age)
+console.log(typeof age);
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+
+
+
+// NaN is not a Number
+
+let no =123
+let stringnumber=String(no)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
