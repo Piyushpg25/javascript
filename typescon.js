c72819f0c8bb658de0adde26d100c4eaf53dee28
Piyush  Gajghate <131654427+Piyushpg25@users.noreply.github.com>
2024-11-04 14:59:37 +0000

    typescon.js

--- typescon.js
+++ typescon.js
@@ -16,4 +16,40 @@ let stringnumber=String(no)
 console.log(stringnumber);
 console.log(typeof stringnumber);
 
+                // opertions
+
+let value=25
+let negvalue=-value-5
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+console.log(2==2);
+console.log(2<=4);
+console.log(2>=8);
+
+let str1="hello"
+let str2=" piyush"
+let str3= str1+str2
+console.log(str3);
+
+// code should be readable,clean,simple.
+// do not write confussion code otherwise code will be not taken by 
+
+
+let x=3
+let y=x++
+console.log(x);
+
+let p=3
+let g=p--
+console.log(p);
+
+
+
+
 
