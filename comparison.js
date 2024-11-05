bfd8f91c3185654dd49ec777e4eeb3d5c07e622f
Piyush  Gajghate <131654427+Piyushpg25@users.noreply.github.com>
2024-11-05 05:20:05 +0000

    comparison.js

---
+++ comparison.js
@@ -0,0 +1,16 @@
+console.log(2>1);
+console.log(2==1);
+console.log(2>=1);
+console.log(2!=1);
+
+// === check value strictly
+console.log("2"===2);
+console.log(false>1);
+// avoid this type of comparison
+
+console.log(null>0);
+console.log(null<=0);
+console.log(null==0);
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
